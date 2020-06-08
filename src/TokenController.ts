4bb9be98f8540775222a13935025225b169acd1b
Danny Garber <dannyg@microsoft.com>
2020-06-08 14:19:33 -0400

    updated project files for local dev env

--- src/TokenController.ts
+++ src/TokenController.ts
@@ -1,80 +1,81 @@
-import { DefaultAzureCredential, TokenCredentialOptions } from '@azure/identity';
-import { BlobServiceClient, ContainerClient } from '@azure/storage-blob';
-import express from 'express';
-
-
+import {
+  DefaultAzureCredential,
+  TokenCredentialOptions,
+} from "@azure/identity";
+import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
+import express from "express";
 
 /** this route provides token signing for testing only. */
 // class SignController {
-//   private status = 'use POST with a json jwt payload';
+//   private status = "use POST with a json jwt payload";
 //   public router = express.Router();
 
 //   constructor() {
-//     this.router.get('/sign', this.getSign);
-//     this.router.post('/sign', this.sign);
+//     this.router.get("/sign", this.getSign);
+//     this.router.post("/sign", this.sign);
 //   }
 
-
-
 /**
- * For a given blob object, generator a SAS Token that'll let bearers access the blob for t hours.
+ * For a given blob object, generator a SAS Token that"ll let bearers access the blob for t hours.
  */
 export class TokenController {
-    defaultCredential: DefaultAzureCredential;
-    public router = express.Router();
-    private status = 'use POST with a json jwt payload'; //TODO: remove
-    // to: TokenCredentialOptions = {
-    //     ""
-    // };
-    constructor() {
-        this.defaultCredential = new DefaultAzureCredential();
-        this.router.post('/sign', this.sign);
-        this.router.get('/sign', this.getSign);
-    }
+  private defaultCredential: DefaultAzureCredential;
+  public router = express.Router();
+  private status = "use POST with a json jwt payload"; // TODO: remove
+  // to: TokenCredentialOptions = {
+  //     ""
+  // };
+  constructor() {
+    this.defaultCredential = new DefaultAzureCredential();
+    this.router.post("/sign", this.sign);
+    this.router.get("/sign", this.getSign);
+  }
 
-    getSign = (request: express.Request, response: express.Response) => {
-        return response.send(this.status);
-    };
+  getSign = (request: express.Request, response: express.Response) => {
+    return response.send(this.status);
+  };
 
-    sign = (request: express.Request, response: express.Response) => {
-        // const options: SignOptions = { algorithm: 'HS256'}
-        // const rv = jwt.sign(request.body, sharedSecret, options)
-        this.signBlob('dkdkdk');
-        return response.send('sign NOTR');
-    };
+  sign = (request: express.Request, response: express.Response) => {
+    // const options: SignOptions = { algorithm: "HS256"}
+    // const rv = jwt.sign(request.body, sharedSecret, options)
+    this.signBlob("dkdkdk");
+    return response.send("sign NOTR");
+  };
 
-    // private String calcBlobAccountUrl(BlobUrlParts parts) {
-    //     return String.format("https://%s.blob.core.windows.net", parts.getAccountName());
-    // }
+  // private String calcBlobAccountUrl(BlobUrlParts parts) {
+  //     return String.format("https://%s.blob.core.windows.net", parts.getAccountName());
+  // }
 
-    // private OffsetDateTime calcTokenExpirationDate() {
-    //     return OffsetDateTime.now(ZoneOffset.UTC).plusHours(12);
-    // }
+  // private OffsetDateTime calcTokenExpirationDate() {
+  //     return OffsetDateTime.now(ZoneOffset.UTC).plusHours(12);
+  // }
 
-    // public signContainer(containerUrl: string) : string {
+  // public signContainer(containerUrl: string) : string {
 
-    // }
+  // }
 
-    private signBlob(blobUrl: string): string {
-        const client: BlobServiceClient = new BlobServiceClient(blobUrl, this.defaultCredential);
+  private signBlob(blobUrl: string): string {
+    const client: BlobServiceClient = new BlobServiceClient(
+      blobUrl,
+      this.defaultCredential
+    );
 
-        const iss: Date = new Date()
-        const exp: Date = new Date();//timeinterval of 1 hour needed.
+    const iss: Date = new Date();
+    const exp: Date = new Date(); // timeinterval of 1 hour needed.
 
-        const s = '';
-        const k = this.defaultCredential.getToken('')
-            .then(token => {
-                client.getUserDelegationKey(iss, exp);
-            })
-            .then(sas => {
+    const s = "";
+    const k = this.defaultCredential
+      .getToken("")
+      .then((token) => {
+        client.getUserDelegationKey(iss, exp);
+      })
+      .then((sas) => {
+        console.log(sas);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
-            })
-            .catch(err => {
-                console.error(err);
-            });
-
-
-        return '';
-
-    }
+    return "";
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,150 +1,131 @@
 /* eslint-disable no-console */
-import dotenv from 'dotenv';
-import express, { Application, Request, Response, NextFunction, RequestHandler } from 'express';
-import bodyParser from 'body-parser';
-
-import {TokenController} from './TokenController';
+import express, {
+  Application,
+  Request,
+  Response,
+  NextFunction,
+  RequestHandler,
+} from "express";
+import bodyParser from "body-parser"; // to convert the body of incoming requests into JavaScript objects.
+import dotenv from "dotenv"; // to read and load the environment variables from .env file
+import envalid from "envalid"; // to validate the environment variables presence
+import cors from "cors"; //  to configure Express to add CORS
+import helmet from "helmet"; // to secure Express APIs by defining various HTTP headers.
+import morgan from "morgan"; // adds some logging capabilities to this Express API.
+import { StatusController } from "./StatusController";
+import { ClientController } from "./ClientController";
+import { TokenController } from "./TokenController";
 
 /** This is a demo server used to test and validate the actual middleware which is in index.ts */
 
-
 dotenv.config();
 
-// https://dev.to/aligoren/developing-an-express-application-using-typescript-3b1
+const env = envalid.cleanEnv(process.env, {
+  PORT: envalid.port({
+    example: "4000",
+    default: 4000,
+    desc: "The localhost port on which this sample would run",
+  }),
+});
 
 const loggerMiddleware = (req: Request, resp: Response, next: NextFunction) => {
-  console.log('Request logged:', req.method, req.path);
+  console.log("Request logged:", req.method, req.path);
   next();
-}
+};
 
 const sharedSecret = "notasecret";
 
-/** this route is to be protected */
-class UserController {
-  private sampleUsers:string[] = ['john', 'paul', 'ringo'];
-  public router = express.Router();
-
-  constructor() {
-    this.router.get('/users', this.getAllUsers);
-  }
-
-  getAllUsers = (request: express.Request, response: express.Response) => {
-    return response.send(this.sampleUsers);
-  };
-}
-
-/** this route is OPEN  */
-class StatusController {
-  private status = 'all OK';
-  public router = express.Router();
-
-  constructor() {
-    this.router.get('/status', this.getStatus);
-  }
-
-  getStatus = (request: express.Request, response: express.Response) => {
-    return response.send(this.status);
-  };
-}
-
-/** this route provides token signing for testing only. */
-// class SignController {
-//   private status = 'use POST with a json jwt payload';
-//   public router = express.Router();
-
-//   constructor() {
-//     this.router.get('/sign', this.getSign);
-//     this.router.post('/sign', this.sign);
-//   }
-
-//   getSign = (request: express.Request, response: express.Response) => {
-//     return response.send(this.status);
-//   };
-
-//   sign = (request: express.Request, response: express.Response) => {
-//     // const options: SignOptions = { algorithm: 'HS256'}
-//     // const rv = jwt.sign(request.body, sharedSecret, options)
-//     return response.send('sign NOTR');
-//   };
-
-// }
-
 class App {
-  public app: Application
-  public port: number
+  public app: Application;
+  public port: number;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  constructor (appInit: { port: number; middleWares: RequestHandler[]; controllers: any; }) {
-    this.app = express()
-    this.port = appInit.port
+  constructor(appInit: {
+    port: number;
+    middleWares: RequestHandler[];
+    controllers: any;
+  }) {
+    this.app = express();
+    this.port = appInit.port;
 
-    this.middlewares(appInit.middleWares)
-    this.routes(appInit.controllers)
-    this.assets()
-    this.template()
+    // adding Helmet to enhance API's security
+    this.app.use(helmet());
+
+    // enabling CORS for all requests
+    this.app.use(cors());
+
+    // adding morgan to log HTTP requests
+    this.app.use(morgan('combined'));
+
+    this.middlewares(appInit.middleWares);
+    this.routes(appInit.controllers);
+    this.assets();
+    this.template();
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private middlewares(middleWares: { forEach: (arg0: (middleWare: any) => void) => void; }) {
-    middleWares.forEach(middleWare => {
-      this.app.use(middleWare)
-    })
+  private middlewares(middleWares: {
+    forEach: (arg0: (middleWare: any) => void) => void;
+  }) {
+    middleWares.forEach((middleWare) => {
+      this.app.use(middleWare);
+    });
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private routes (controllers: { forEach: (arg0: (controller: any) => void) => void; }) {
-    controllers.forEach(controller => {
-      this.app.use('/', controller.router)
-    })
+  private routes(controllers: {
+    forEach: (arg0: (controller: any) => void) => void;
+  }) {
+    controllers.forEach((controller) => {
+      this.app.use("/", controller.router);
+    });
   }
 
-  private assets () {
-    //this.app.use(express.static('public'))
-    //this.app.use(express.static('views'))
+  private assets() {
+    // this.app.use(express.static("public"))
+    // this.app.use(express.static("views"))
   }
 
-  private template () {
-    //this.app.set('view engine', 'pug')
+  private template() {
+    // this.app.set("view engine", "pug")
   }
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  public listen () {
+  public listen() {
     this.app.listen(this.port, () => {
-      console.log(`App listening on the http://localhost:${this.port}`)
-    })
+      console.log(`App listening on the http://localhost:${this.port}`);
+    });
   }
 }
 
 const appWrapper = new App({
-  port: (!process.env.PORT) ? 5000 : parseInt(process.env.PORT),
+  port: !env.PORT ? 4000 : env.PORT,
   controllers: [
-    new UserController(),
+    new ClientController(),
     new StatusController(),
-    new TokenController()
+    new TokenController(),
   ],
   middleWares: [
     loggerMiddleware,
     bodyParser.json(),
     // bodyParser.urlencoded({ extended: true })
-  ]
-})
+  ],
+});
 
-
-//test if running in mocha
-const isInTest = false; //typeof global.it === 'function';
-//running as a proper listener process NOT under Mocha
-if (!isInTest)
-{ 
+// test if running in mocha
+const isInTest = false; // typeof global.it === "function";
+// running as a proper listener process NOT under Mocha
+if (!isInTest) {
   appWrapper.listen();
-}
-else {
-  //running under mocha, just forward the status code; suppress the stack trace
-  appWrapper.app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-    //console.error(error);
-    res.sendStatus(res.statusCode);
-    next();
-  });
+} else {
+  // running under mocha, just forward the status code; suppress the stack trace
+  appWrapper.app.use(
+    (error: Error, req: Request, res: Response, next: NextFunction) => {
+      // console.error(error);
+      res.sendStatus(res.statusCode);
+      next();
+    }
+  );
 }
 
 export default appWrapper.app;
-
